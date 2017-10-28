94abd181ff9a3d36ee1399dc04af47c5ad0bb3f9
Soul Evans <soulevans07@gmail.com>
2017-10-28 21:11:18 +0200

    Added jump animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
 	// moving and colliding
-	public float maxSpeed = 5f;
+	public float maxSpeed = 5.0f;
 	private bool facingRight = true;
 	private Rigidbody2D rd2d;
 	private Animator anim;
@@ -16,17 +17,23 @@ public class PlayerController : MonoBehaviour {
 	private bool grounded = false;
 	private float groundRadius = 0.2f;
 
-	void Start () {
+	// jump
+	public float jumpForce = 80.0f;
+
+	void Start ()
+	{
 		rd2d = GetComponent<Rigidbody2D> ();
 		anim = GetComponent<Animator> ();
 	}
-	
-	void FixedUpdate () {
+
+	void FixedUpdate ()
+	{
 		grounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, whatIsGround);
 		anim.SetBool ("player-ground", grounded);
+		anim.SetFloat ("player-y-speed", rd2d.velocity.y);
 
 		float move = Input.GetAxis ("Horizontal");
-		anim.SetFloat("player-x-speed", Mathf.Abs(move));
+		anim.SetFloat ("player-x-speed", Mathf.Abs (move));
 		rd2d.velocity = new Vector2 (move * maxSpeed, rd2d.velocity.y);
 
 		if (move > 0 && !facingRight)
@@ -35,7 +42,16 @@ public class PlayerController : MonoBehaviour {
 			Flip ();
 	}
 
-	void Flip(){
+	void Update ()
+	{
+		if (grounded && Input.GetButton ("Jump")) {
+			anim.SetBool ("player-ground", false);
+			rd2d.AddForce (new Vector2 (0, jumpForce));
+		}
+	}
+
+	void Flip ()
+	{
 		facingRight = !facingRight;
 		Vector3 theScale = transform.localScale;
 		theScale.x *= -1;

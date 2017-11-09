6762a8963c9c954a9fae4f15bfd044482a4b3072
Soul Evans <soulevans07@gmail.com>
2017-11-09 21:47:07 +0100

    atlasToSprite now works on multiple atlas per run

--- atlasToSprite/WorkingMain.java
+++ atlasToSprite/WorkingMain.java
@@ -2,65 +2,107 @@ import java.io.*;
 import java.util.*;
 
 public class WorkingMain{
-	public static int imageWidth = 2048;
-	public static int imageHeight = 4096;
-	public static int startnum = 21300000;
-	public static String fname = "simple.txt";
-	public static ArrayList<Sprite> sprites = new ArrayList<>();
+	public static int imgWidth;
+	public static int imgHeight;
+	public static int START_NUM = 21300000;
+	public static String inputdir = "input";
 	
 	public static void main(String[] args) {
-		loadSprites();
-		generateSpriteAtlas();
+		if(args.length != 0)
+			inputdir = args[0];
+
+		for(File atlas : searchByExt(inputdir, ".atlas")){
+			String baseName = getBaseName(atlas.getName());
+			StringBuilder meta = new StringBuilder();
+			meta.append(getMetaBase(inputdir, baseName+".png.meta"));
+			generateSpriteAtlas(meta, loadSprites(inputdir, atlas.getName()));
+			saveMeta(meta, inputdir, baseName+".png.meta");
+		}
 	}
 
+	public static void saveMeta(StringBuilder meta, String dir, String file){
+		try{
+			BufferedWriter writer = new BufferedWriter(new FileWriter(dir+"/"+file));
+			writer.write(meta.toString());
+			writer.close();
+		} catch(IOException e) {
+			System.out.println(e);
+		}
+	}
 
+	public static String getBaseName(String filename){
+		String[] parts = filename.split("\\.");
+		return parts[0];
+	}
+
+	public static File[] searchByExt(String dir, String fileExt){
+		File folder = new File(dir);
+		File[] matching = folder.listFiles(new FilenameFilter(){
+			public boolean accept(File dir, String name){
+				return name.endsWith(fileExt);
+			}
+		});
+		return matching;
+	}
+
+	public static String getMetaBase(String dir, String metafile){
+		String meta = getLines(dir, metafile);
+		String[] split = meta.split("\r\n");
+		StringBuilder base = new StringBuilder();
+		for(int i = 0; i < 3; i++)
+			base.append(split[i]).append('\n');
+		return base.toString();
+	}
 
 	public static void log(Object o){
 		System.out.print(o+"\n");
 	}
 
-	public static void loadSprites(){
-//		try {
-//			String content = new Scanner(new File(fname)).useDelimiter("\\Z").next();
-			String content = getLines(fname);
+	public static ArrayList<Sprite> loadSprites(String dir, String filename){
+		ArrayList<Sprite> sprites = new ArrayList<>();
+		String content = getLines(dir, filename);
 
-			String[] split = content.split("\r\n");
+		String[] split = content.split("\r\n");
 
-			for(int i = 0; i < split.length; i+=7){
-				Sprite tmp = new Sprite();
-				tmp.name = split[i];				
+		String imgSize = split[2].replace("size: ", "");
+		imgWidth = Integer.parseInt(imgSize.split(",")[0]);
+		imgHeight = Integer.parseInt(imgSize.split(",")[1]);
 
-				String rotate = split[i+1].replace("  rotate: ", "");
-				tmp.rotate = Boolean.parseBoolean(rotate);
+		for(int i = 6; i < split.length; i+=7){
+			Sprite tmp = new Sprite();
+			tmp.name = split[i];				
 
-				String xy = split[i+2].replace("  xy: ", "");
-				tmp.x = Integer.parseInt(xy.split(", ")[0]);
-				tmp.y = Integer.parseInt(xy.split(", ")[1]);
+			String rotate = split[i+1].replace("  rotate: ", "");
+			tmp.rotate = Boolean.parseBoolean(rotate);
 
-				String size = split[i+3].replace("  size: ", "");
-				tmp.w = Integer.parseInt(size.split(", ")[0]);
-				tmp.h = Integer.parseInt(size.split(", ")[1]);
+			String xy = split[i+2].replace("  xy: ", "");
+			tmp.x = Integer.parseInt(xy.split(", ")[0]);
+			tmp.y = Integer.parseInt(xy.split(", ")[1]);
 
-				String orig = split[i+4].replace("  orig: ", "");
-				tmp.origX = Integer.parseInt(orig.split(", ")[0]);
-				tmp.origY = Integer.parseInt(orig.split(", ")[1]);
+			String size = split[i+3].replace("  size: ", "");
+			tmp.w = Integer.parseInt(size.split(", ")[0]);
+			tmp.h = Integer.parseInt(size.split(", ")[1]);
 
-				String off = split[i+5].replace("  offset: ", "");
-				tmp.offX = Integer.parseInt(off.split(", ")[0]);
-				tmp.offY = Integer.parseInt(off.split(", ")[1]);
+			String orig = split[i+4].replace("  orig: ", "");
+			tmp.origX = Integer.parseInt(orig.split(", ")[0]);
+			tmp.origY = Integer.parseInt(orig.split(", ")[1]);
 
-				String index = split[i+6].replace("  index: ", "");
-				tmp.index = Integer.parseInt(index);
+			String off = split[i+5].replace("  offset: ", "");
+			tmp.offX = Integer.parseInt(off.split(", ")[0]);
+			tmp.offY = Integer.parseInt(off.split(", ")[1]);
 
-				sprites.add(tmp);
-			}
-//		} catch (FileNotFoundException e) {
-//			e.printStackTrace();
-//		}
+			String index = split[i+6].replace("  index: ", "");
+			tmp.index = Integer.parseInt(index);
+
+			sprites.add(tmp);
+		}
+
+		return sprites;
 	}
 
-	public static String getLines(String fileName){
-		try (BufferedReader br = new BufferedReader(new FileReader(fileName))){
+	public static String getLines(String dirName, String fileName){
+		String path = (dirName != null || dirName.equals("")) ? dirName + "/" + fileName : fileName;
+		try (BufferedReader br = new BufferedReader(new FileReader(path))){
 		    StringBuilder sb = new StringBuilder();
 		    String line = br.readLine();
 
@@ -99,113 +141,109 @@ public class WorkingMain{
 		}
 	}
 
-	public static void generateSpriteAtlas(){
-		log("fileFormatVersion: 2");
-		log("guid: 8a438e08ceed56b4eb9353b25c127d1c");
-		log("timeCreated: 1508015774");
-		log("licenseType: Free");
-		log("TextureImporter:");
-		log("  fileIDToRecycleName:");
+	public static void generateSpriteAtlas(StringBuilder meta, ArrayList<Sprite> sprites){
+		meta.append("TextureImporter:").append('\n');
+		meta.append("  fileIDToRecycleName:").append('\n');
 
 		int i = 0;
 		for(Sprite s : sprites){
-			log("    " + (startnum+i) + ": " + s.name);
+			meta.append("    " + (START_NUM+i) + ": " + s.name).append('\n');
 			i+=2;
 		}
 
-		log("  serializedVersion: 4");
-		log("  mipmaps:");
-		log("    mipMapMode: 0");
-		log("    enableMipMap: 0");
-		log("    sRGBTexture: 1");
-		log("    linearTexture: 0");
-		log("    fadeOut: 0");
-		log("    borderMipMap: 0");
-		log("    mipMapsPreserveCoverage: 0");
-		log("    alphaTestReferenceValue: 0.5");
-		log("    mipMapFadeDistanceStart: 1");
-		log("    mipMapFadeDistanceEnd: 3");
-		log("  bumpmap:");
-		log("    convertToNormalMap: 0");
-		log("    externalNormalMap: 0");
-		log("    heightScale: 0.25");
-		log("    normalMapFilter: 0");
-		log("  isReadable: 0");
-		log("  grayScaleToAlpha: 0");
-		log("  generateCubemap: 6");
-		log("  cubemapConvolution: 0");
-		log("  seamlessCubemap: 0");
-		log("  textureFormat: 1");
-		log("  maxTextureSize: 2048");
-		log("  textureSettings:");
-		log("    serializedVersion: 2");
-		log("    filterMode: 0");
-		log("    aniso: -1");
-		log("    mipBias: -1");
-		log("    wrapU: 1");
-		log("    wrapV: 1");
-		log("    wrapW: 1");
-		log("  nPOTScale: 0");
-		log("  lightmap: 0");
-		log("  compressionQuality: 50");
-		log("  spriteMode: 2");
-		log("  spriteExtrude: 1");
-		log("  spriteMeshType: 1");
-		log("  alignment: 0");
-		log("  spritePivot: {x: 0.5, y: 0.5}");
-		log("  spriteBorder: {x: 0, y: 0, z: 0, w: 0}");
-		log("  spritePixelsToUnits: 100");
-		log("  alphaUsage: 1");
-		log("  alphaIsTransparency: 1");
-		log("  spriteTessellationDetail: -1");
-		log("  textureType: 8");
-		log("  textureShape: 1");
-		log("  maxTextureSizeSet: 0");
-		log("  compressionQualitySet: 0");
-		log("  textureFormatSet: 0");
-		log("  platformSettings:");
-		log("  - buildTarget: DefaultTexturePlatform");
-		log("    maxTextureSize: 8192");
-		log("    textureFormat: -1");
-		log("    textureCompression: 0");
-		log("    compressionQuality: 50");
-		log("    crunchedCompression: 0");
-		log("    allowsAlphaSplitting: 0");
-		log("    overridden: 0");
-		log("  - buildTarget: Standalone");
-		log("    maxTextureSize: 8192");
-		log("    textureFormat: -1");
-		log("    textureCompression: 0");
-		log("    compressionQuality: 50");
-		log("    crunchedCompression: 0");
-		log("    allowsAlphaSplitting: 0");
-		log("    overridden: 0");
-		log("  spriteSheet:");
-		log("    serializedVersion: 2");
-		log("    sprites:");
+		meta.append("  serializedVersion: 4").append('\n');
+		meta.append("  mipmaps:").append('\n');
+		meta.append("    mipMapMode: 0").append('\n');
+		meta.append("    enableMipMap: 0").append('\n');
+		meta.append("    sRGBTexture: 1").append('\n');
+		meta.append("    linearTexture: 0").append('\n');
+		meta.append("    fadeOut: 0").append('\n');
+		meta.append("    borderMipMap: 0").append('\n');
+		meta.append("    mipMapsPreserveCoverage: 0").append('\n');
+		meta.append("    alphaTestReferenceValue: 0.5").append('\n');
+		meta.append("    mipMapFadeDistanceStart: 1").append('\n');
+		meta.append("    mipMapFadeDistanceEnd: 3").append('\n');
+		meta.append("  bumpmap:").append('\n');
+		meta.append("    convertToNormalMap: 0").append('\n');
+		meta.append("    externalNormalMap: 0").append('\n');
+		meta.append("    heightScale: 0.25").append('\n');
+		meta.append("    normalMapFilter: 0").append('\n');
+		meta.append("  isReadable: 0").append('\n');
+		meta.append("  grayScaleToAlpha: 0").append('\n');
+		meta.append("  generateCubemap: 6").append('\n');
+		meta.append("  cubemapConvolution: 0").append('\n');
+		meta.append("  seamlessCubemap: 0").append('\n');
+		meta.append("  textureFormat: 1").append('\n');
+		meta.append("  maxTextureSize: 2048").append('\n');
+		meta.append("  textureSettings:").append('\n');
+		meta.append("    serializedVersion: 2").append('\n');
+		meta.append("    filterMode: 0").append('\n');
+		meta.append("    aniso: -1").append('\n');
+		meta.append("    mipBias: -1").append('\n');
+		meta.append("    wrapU: 1").append('\n');
+		meta.append("    wrapV: 1").append('\n');
+		meta.append("    wrapW: 1").append('\n');
+		meta.append("  nPOTScale: 0").append('\n');
+		meta.append("  lightmap: 0").append('\n');
+		meta.append("  compressionQuality: 50").append('\n');
+		meta.append("  spriteMode: 2").append('\n');
+		meta.append("  spriteExtrude: 1").append('\n');
+		meta.append("  spriteMeshType: 1").append('\n');
+		meta.append("  alignment: 0").append('\n');
+		meta.append("  spritePivot: {x: 0.5, y: 0.5}").append('\n');
+		meta.append("  spriteBorder: {x: 0, y: 0, z: 0, w: 0}").append('\n');
+		meta.append("  spritePixelsToUnits: 100").append('\n');
+		meta.append("  alphaUsage: 1").append('\n');
+		meta.append("  alphaIsTransparency: 1").append('\n');
+		meta.append("  spriteTessellationDetail: -1").append('\n');
+		meta.append("  textureType: 8").append('\n');
+		meta.append("  textureShape: 1").append('\n');
+		meta.append("  maxTextureSizeSet: 0").append('\n');
+		meta.append("  compressionQualitySet: 0").append('\n');
+		meta.append("  textureFormatSet: 0").append('\n');
+		meta.append("  platformSettings:").append('\n');
+		meta.append("  - buildTarget: DefaultTexturePlatform").append('\n');
+		meta.append("    maxTextureSize: 8192").append('\n');
+		meta.append("    textureFormat: -1").append('\n');
+		meta.append("    textureCompression: 0").append('\n');
+		meta.append("    compressionQuality: 50").append('\n');
+		meta.append("    crunchedCompression: 0").append('\n');
+		meta.append("    allowsAlphaSplitting: 0").append('\n');
+		meta.append("    overridden: 0").append('\n');
+		meta.append("  - buildTarget: Standalone").append('\n');
+		meta.append("    maxTextureSize: 8192").append('\n');
+		meta.append("    textureFormat: -1").append('\n');
+		meta.append("    textureCompression: 0").append('\n');
+		meta.append("    compressionQuality: 50").append('\n');
+		meta.append("    crunchedCompression: 0").append('\n');
+		meta.append("    allowsAlphaSplitting: 0").append('\n');
+		meta.append("    overridden: 0").append('\n');
+		meta.append("  spriteSheet:").append('\n');
+		meta.append("    serializedVersion: 2").append('\n');
+		meta.append("    sprites:").append('\n');
 
     	for(Sprite s : sprites){
-    		log("    - serializedVersion: 2");
-		    log("      name: " + s.name);
-		    log("      rect:");
-		    log("        serializedVersion: 2");
-		    log("        x: " + s.x);
-		    log("        y: " + (imageHeight - s.y - s.h));
-		    log("        width: " + s.w);
-		    log("        height: " + s.h);
-		    log("      alignment: 0");
-		    log("      pivot: {x: 0, y: 0}");
-		    log("      border: {x: 0, y: 0, z: 0, w: 0}");
-		    log("      outline: []");
-		    log("      physicsShape: []");
-		    log("      tessellationDetail: 0");
+    		meta.append("    - serializedVersion: 2").append('\n');
+		    meta.append("      name: " + s.name).append('\n');
+		    meta.append("      rect:").append('\n');
+		    meta.append("        serializedVersion: 2").append('\n');
+		    meta.append("        x: " + s.x).append('\n');
+		    meta.append("        y: " + (imgHeight - s.y - s.h)).append('\n');
+		    meta.append("        width: " + s.w).append('\n');
+		    meta.append("        height: " + s.h).append('\n');
+		    meta.append("      alignment: 0").append('\n');
+		    meta.append("      pivot: {x: 0, y: 0}").append('\n');
+		    meta.append("      border: {x: 0, y: 0, z: 0, w: 0}").append('\n');
+		    meta.append("      outline: []").append('\n');
+		    meta.append("      physicsShape: []").append('\n');
+		    meta.append("      tessellationDetail: 0").append('\n');
     	}
 
-    	log("    outline: []");
-		log("    physicsShape: []");
-		log("  spritePackingTag: ");
-		log("  userData: ");
-		log("  assetBundleName: ");
-		log("  assetBundleVariant: ");
+    	meta.append("    outline: []").append('\n');
+		meta.append("    physicsShape: []").append('\n');
+		meta.append("  spritePackingTag: ").append('\n');
+		meta.append("  userData: ").append('\n');
+		meta.append("  assetBundleName: ").append('\n');
+		meta.append("  assetBundleVariant: ").append('\n');
 	}
 }

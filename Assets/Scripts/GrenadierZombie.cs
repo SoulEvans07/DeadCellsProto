8d1cef36cf0fa8de3b09baf3b636020b716a391d
Soul Evans <soulevans07@gmail.com>
2017-11-23 17:09:04 +0100

    added more animation and a few enemy interactions

---
+++ Assets/Scripts/GrenadierZombie.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrenadierZombie : MonoBehaviour {
+	// life
+	public int health = 100;
+	private bool dead = false;
+
+	// moving and colliding
+	public float maxSpeed = 2.8f;
+	private bool facingRight = true;
+	private Rigidbody2D rd2d;
+	private Animator anim;
+	private SpriteRenderer spriteRenderer;
+
+	// attack
+	private bool attack = false;
+	public float attackDistance = 0.4f;
+	public Vector3 slashFxOffset = new Vector3(0.213f, 0.056f, 0);
+	private Vector3 slashFxOffsetLeft;
+	public GameObject slashAttackFx;
+
+	void Start () {
+		rd2d = GetComponent<Rigidbody2D> ();
+		anim = GetComponent<Animator> ();
+		spriteRenderer = GetComponent<SpriteRenderer> ();
+		// attack
+		slashFxOffsetLeft = slashFxOffset;
+		slashFxOffsetLeft.x *= -1;
+	}
+	
+
+	void FixedUpdate (){
+		if (attack) {
+			return;
+		}
+
+		SetBoxCollider ();
+		if (dead) {
+			Die ();
+			return;
+		}
+
+
+		// live
+		Transform playerTrans = PlayerController.instance.transform;
+		float diffX = playerTrans.position.x - transform.position.x;
+		float diffY = playerTrans.position.y - transform.position.y;
+		if(0 < diffX && diffX < attackDistance && Mathf.Abs(diffY) < 0.1f){
+			StartCoroutine(Attack());
+		}
+
+
+		//if (move > 0 && !facingRight)
+		//	Flip ();
+		//else if (move < 0 & facingRight)
+		//	Flip ();
+	}
+
+	IEnumerator Attack(){
+		attack = true;
+
+		SpawnSlashFx();
+		anim.SetBool ("zombie-attack", true);
+		yield return new WaitForSeconds (0.8f);
+		anim.SetBool ("zombie-attack", false);
+
+		attack = false;
+	}
+
+	void SpawnSlashFx(){
+		GameObject slash;
+		if (facingRight) {
+			slash = Instantiate(slashAttackFx, transform.position+slashFxOffset, transform.rotation) as GameObject;
+		} else {
+			slash = Instantiate(slashAttackFx, transform.position+slashFxOffsetLeft, transform.rotation) as GameObject;
+			slash.transform.parent = gameObject.transform;
+			Vector3 theScale = slash.transform.localScale;
+			theScale.x *= -1;
+			slash.transform.localScale = theScale;
+		}
+		if (slash != null) {
+			slash.transform.parent = gameObject.transform;
+			Destroy (slash, 1f);
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		health -= 10;
+		if (!other.CompareTag ("PlayerAtk"))
+			return;
+		anim.SetTrigger ("zombie-hit");
+
+		if (health <= 0)
+			dead = true;
+
+		Debug.Log ("zombie[ hp: " + health + " ]");
+	}
+
+	void Die(){
+		// play animation
+		anim.SetBool("zombie-dead", dead);
+		Destroy (gameObject, 1f);
+	}
+
+	void Flip ()
+	{
+		facingRight = !facingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+
+	void SetBoxCollider ()
+	{
+		BoxCollider2D itemBoxCollider2D = GetComponent<BoxCollider2D> ();
+		if (itemBoxCollider2D != null) {
+			itemBoxCollider2D.size = spriteRenderer.sprite.bounds.size;
+			itemBoxCollider2D.size.Scale(new Vector3(0.9f, 0.9f, 0.9f));
+		}
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+	// Singleton
+	public static PlayerController instance;
 
 	// moving and colliding
-	public float maxSpeed = 5.0f;
+	public float maxSpeed = 2.8f;
 	private bool facingRight = true;
 	private Rigidbody2D rd2d;
 	private Animator anim;
@@ -27,47 +29,90 @@ public class PlayerController : MonoBehaviour
 	private bool crouch;
 	private SpriteRenderer spriteRenderer;
 
+	// attack
+	private bool attack = false;
+	public Vector3 saberFxOffset = new Vector3(0.213f, 0.056f, 0);
+	private Vector3 saberFxOffsetLeft;
+	public GameObject saberAttackFx;
+
+	void Awake(){
+		if (instance != null)
+			Debug.LogWarning ("more than one instance");
+		instance = this;
+	}
+
 	void Start ()
 	{
 		rd2d = GetComponent<Rigidbody2D> ();
 		anim = GetComponent<Animator> ();
 		spriteRenderer = GetComponent<SpriteRenderer> ();
 		anim.SetBool ("player-doublejump", doubleJumped);
+		// attack
+		saberFxOffsetLeft = saberFxOffset;
+		saberFxOffsetLeft.x *= -1;
 	}
 
 	void FixedUpdate ()
 	{
-		grounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, whatIsGround);
-		if (grounded && doubleJumped) {
-			doubleJumped = false;
-			stomped = false;
-			anim.SetBool ("player-doublejump", doubleJumped);
-			anim.SetBool ("player-stomp", doubleJumped);
-		}
+		if (attack) {
+			anim.SetTrigger ("player-attackX");
+			GameObject slash;
+			if (facingRight) {
+				slash = Instantiate(saberAttackFx, transform.position+saberFxOffset, transform.rotation) as GameObject;
+			} else {
+				slash = Instantiate(saberAttackFx, transform.position+saberFxOffsetLeft, transform.rotation) as GameObject;
+				slash.transform.parent = gameObject.transform;
+				Vector3 theScale = slash.transform.localScale;
+				theScale.x *= -1;
+				slash.transform.localScale = theScale;
+			}
+			if (slash != null) {
+				slash.transform.parent = gameObject.transform;
+				Destroy (slash, 1f);
+			}
+			//Debug.Log ("attack: " + attack);
+			//WaitForAnim("AtkBackStabberB");
+			attack = false;
+			//Debug.Log ("attack: " + attack);
+		} else {
+
+			grounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, whatIsGround);
+			if (grounded && doubleJumped) {
+				doubleJumped = false;
+				stomped = false;
+				anim.SetBool ("player-doublejump", doubleJumped);
+				anim.SetBool ("player-stomp", doubleJumped);
+			}
 		
-		anim.SetBool ("player-ground", grounded);
-		anim.SetFloat ("player-y-speed", rd2d.velocity.y);
+			anim.SetBool ("player-ground", grounded);
+			anim.SetFloat ("player-y-speed", rd2d.velocity.y);
 
-		float vertical = Input.GetAxis ("Vertical");
-		crouch = grounded && (vertical < 0);
-		anim.SetBool ("player-crouch", crouch);
+			float vertical = Input.GetAxis ("Vertical");
+			crouch = grounded && (vertical < 0);
+			anim.SetBool ("player-crouch", crouch);
 
-		float move = Input.GetAxis ("Horizontal");
-		if (!crouch) {
-			anim.SetFloat ("player-x-speed", Mathf.Abs (move));
-			rd2d.velocity = new Vector2 (move * maxSpeed, rd2d.velocity.y);
+			float move = Input.GetAxis ("Horizontal");
+			if (!crouch) {
+				anim.SetFloat ("player-x-speed", Mathf.Abs (move));
+				rd2d.velocity = new Vector2 (move * maxSpeed, rd2d.velocity.y);
+			}
+
+			if (move > 0 && !facingRight)
+				Flip ();
+			else if (move < 0 & facingRight)
+				Flip ();
+
+			SetBoxCollider ();
 		}
-
-		if (move > 0 && !facingRight)
-			Flip ();
-		else if (move < 0 & facingRight)
-			Flip ();
-
-		SetBoxCollider ();
 	}
 
 	void Update ()
 	{
+		if (Input.GetButtonDown ("Fire2")) {
+			attack = true;
+			return;
+		}
+
 		if (grounded && Input.GetButtonDown ("Jump")) {
 			anim.SetBool ("player-ground", false);
 			rd2d.AddForce (new Vector2 (0, jumpForce));
@@ -76,7 +121,8 @@ public class PlayerController : MonoBehaviour
 			rd2d.velocity = new Vector2 (rd2d.velocity.x, 0);
 			rd2d.AddRelativeForce (new Vector2 (0, jumpForce));
 			doubleJumped = true;
-			Instantiate(airDash, groundCheck.position, groundCheck.rotation);
+			GameObject dashFx = Instantiate(airDash, groundCheck.position, groundCheck.rotation) as GameObject;
+			Destroy (dashFx, 2f);
 			anim.SetBool ("player-doublejump", doubleJumped);
 		}
 		if (!grounded && !stomped && Input.GetButtonDown ("Jump") && Input.GetAxis ("Vertical") < 0) {
@@ -85,6 +131,12 @@ public class PlayerController : MonoBehaviour
 			stomped = true;
 			anim.SetBool ("player-stomp", stomped);
 		}
+	}
+
+	void WaitForAnim(string animationName){
+		do {
+			// do nothing
+		} while (anim.GetCurrentAnimatorStateInfo(0).IsName(animationName));
 	}
 
 	void Flip ()

8fad02856726c26c98b8f986f832aa75663fa601
Soul Evans <soulevans07@gmail.com>
2017-11-30 12:58:08 +0100

    added highscore panel

---
+++ Assets/Scripts/HighScoreMenu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class HighScoreMenu : MonoBehaviour {
+
+	public TextMeshProUGUI goldLabel;
+	public TextMeshProUGUI highscoreLabel;
+	public TextMeshProUGUI scoreLabel;
+
+	void Start(){
+		int gold = 0;
+		int highscore = 0;
+
+		if(PlayerPrefs.HasKey("player-gold"))
+			gold = PlayerPrefs.GetInt ("player-gold");
+
+		if(PlayerPrefs.HasKey("highscore"))
+			highscore = PlayerPrefs.GetInt ("highscore");
+
+		if (highscore < gold) {
+			// change text to "new highscore"
+			highscoreLabel.text = "New Highscore!";
+
+			PlayerPrefs.SetInt("highscore", gold);
+		}
+
+		// set player score
+		goldLabel.text = gold.ToString();
+		// set highscore
+		scoreLabel.text = highscore.ToString();
+	}
+
+	public void MainMenu(){
+		SceneManager.LoadScene ("Menu");
+	}
+}

4038b83629532d3af1e973b1149aa38b541115c0
Soul Evans <soulevans07@gmail.com>
2018-05-17 03:50:54 +0200

    Menu navigation done

---
+++ Assets/Scripts/ui/ISaidNoMouse.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ISaidNoMouse : MonoBehaviour {
+    private GameObject selectedObj;
+
+    void Start() {
+        selectedObj = EventSystem.current.currentSelectedGameObject;
+    }
+
+    void Update() {
+        if (EventSystem.current.currentSelectedGameObject == null)
+            EventSystem.current.SetSelectedGameObject(selectedObj);
+
+        selectedObj = EventSystem.current.currentSelectedGameObject;
+    }
+}
